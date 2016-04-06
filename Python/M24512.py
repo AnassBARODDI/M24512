509136a19e02cb6d61f7b369dc566a434185f7fb
Yadwinder Singh <ryker1990@gmail.com>
2016-04-06 11:07:33 +0530

    Update M24512.py

--- Python/M24512.py
+++ Python/M24512.py
@@ -13,8 +13,6 @@ bus = smbus.SMBus(1)
 # M24512 address, 0x50(80)
 # Select write address
 #		0x0001(01)
-
-# M24512 address, 0x50(80)
 # Select write data
 #		0x30(48)
 bus.write_i2c_block_data(0x50, 0x00, [0x01 , 0x30])
@@ -25,9 +23,11 @@ time.sleep(0.5)
 # Select read address
 bus.write_i2c_block_data(0x50, 0x00, [0x01])
 
+time.sleep(0.5)
+
 # M24512 address, 0x50(80)
 # Read data back
 data = bus.read_byte(0x50)
 
 # Output data to screen
-print "Input Data : ",value
+print "Input Data : ",data
